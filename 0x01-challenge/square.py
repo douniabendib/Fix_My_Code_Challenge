2ad48ebbcab80a904e860267108724f4e3e678c0
bendibdounia <bendib.dounia@gmail.com>
2023-12-19 15:05:39 -0800

    square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
+""" Module for square class"""
+
 
 class square():
-    
+    """ Square class """    
     width = 0
     height = 0
 
     
     def __init__(self, *args, **kwargs):
+        """ Instantiation of class """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -15,13 +18,15 @@ class square():
         return self.width * self.width
 
     def PermiterOfMySquare(self):
+        """ Perimeter of my square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """ Printable representation """
         return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
-
+    """create a classe"""
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
